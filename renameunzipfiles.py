786fb3ac2779166f461a43e2a9ff5db94840a081
cadd <kolodziej.robert@gmail.com>
2015-07-27 15:50:00 +0200

    Create renameunzipfiles.py

---
+++ renameunzipfiles.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 11:12:17 2015
+
+@author: robert
+"""
+
+import os
+import datetime
+import zipfile
+
+#Files umbenennen und nach Alt-Ordner kopieren
+for filename in os.listdir("."):
+    if filename.startswith("UMSATZ"):
+        t = os.path.getmtime(filename)
+        v = datetime.datetime.fromtimestamp(t)
+        x = v.strftime('%Y%m%d-%H%M%S')
+        os.rename(filename, "X://01_Immo//Coba-Daten//Pythontest//old//" +
+                  x + "-" + filename)
+
+#Download-Zip entpacken
+zfile = zipfile.ZipFile("C://Users//robert//Downloads//UMSATZ.zip")
+zfile.extractall("C://Users//robert//Downloads")
+zfile.close()
+
+#Entpackte Datei in Coba-Daten-Verzeichnis kopieren
+os.rename("C://Users//robert//Downloads//UMSATZ.txt",
+          "X://01_Immo//Coba-Daten//Pythontest//UMSATZ.txt")
